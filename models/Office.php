b7b3f63d1b81f1be316323d83025e22a400aa736
dzoana94 <joanna3000@onet.pl>
2017-10-09 15:52:10 +0200

    lll

---
+++ models/Office.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "office".
+ *
+ * @property integer $id
+ * @property integer $doctor_id
+ * @property string $name
+ * @property integer $city_id
+ * @property string $street
+ * @property string $postal_code
+ *
+ * @property City $city
+ * @property Doctor $doctor
+ */
+class Office extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'office';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['doctor_id', 'name', 'city_id', 'street', 'postal_code'], 'required'],
+            [['doctor_id', 'city_id'], 'integer'],
+            [['name', 'street'], 'string', 'max' => 40],
+            [['postal_code'], 'string', 'max' => 9],
+            [['city_id'], 'exist', 'skipOnError' => true, 'targetClass' => City::className(), 'targetAttribute' => ['city_id' => 'id']],
+            [['doctor_id'], 'exist', 'skipOnError' => true, 'targetClass' => Doctor::className(), 'targetAttribute' => ['doctor_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'doctor_id' => 'Doctor ID',
+            'name' => 'Name',
+            'city_id' => 'City ID',
+            'street' => 'Street',
+            'postal_code' => 'Postal Code',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCity()
+    {
+        return $this->hasOne(City::className(), ['id' => 'city_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDoctor()
+    {
+        return $this->hasOne(Doctor::className(), ['id' => 'doctor_id']);
+    }
+
+    public function saveOffice($userID)
+    {
+
+        $this->doctor_id = $userID;
+
+        if (!$this->validate()) {
+            return false;
+        }
+
+        $transaction = $this->getDb()->beginTransaction();
+
+        if (!$this->save()) {
+            $transaction->rollBack();
+            return false;
+        }
+
+        $transaction->commit();
+
+        return true;
+
+    }
+}
